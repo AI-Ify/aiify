f7cae79b0b99deb08a119c05bfb18dc5214fcac4
Arthur <arwang5@wisc.edu>
2023-02-20 13:14:26 -0600

    Building website skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
+import React, { useRef, Component } from 'react';
 import './App.css';
+import MenuAppBar from './components/AppBar'
+import { Button, Grid, Switch, TextField } from '@mui/material';
+import { Container } from '@mui/system';
 
 function App() {
+  const textFieldRef = useRef('');
+  let tracks = ["spotify:episode:7makk4oTQel546B0PZlDM5", "spotify:episode:43cbJh4ccRD7lzM2730YK3", "spotify:episode:6I3ZzCxRhRkNqnQNo8AZPV"]
+
+  window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    let element = document.getElementById('embed-iframe');
+    let options = {
+      width: '100%',
+      height: '160',
+      uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+    };
+    let callback = (EmbedController) => {
+      document.querySelectorAll('ul#episodes > li > button').forEach(
+        episode => {
+          episode.addEventListener('click', () => {
+            EmbedController.loadUri(episode.dataset.spotifyId)
+          });
+        })
+    };
+    IFrameAPI.createController(element, options, callback);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuAppBar />
+
+      <Container maxWidth="md">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Basic</Grid>
+          <Grid item>
+            <Switch />
+          </Grid>
+          <Grid item>Advanced</Grid>
+        </Grid>
+
+        <TextField multiline fullWidth rows={5} inputRef={textFieldRef}></TextField>
+
+        <Button variant="contained" disableElevation
+          onClick={() => {
+            console.log(textFieldRef.current.value);
+          }}
+        >Generate!</Button>
+
+        <ul id="episodes">
+          <li>
+            <button data-spotify-id="spotify:episode:7makk4oTQel546B0PZlDM5">
+              My Path to Spotify: Women in Engineering
+            </button>
+          </li>
+          <li>
+            <button data-spotify-id="spotify:episode:43cbJh4ccRD7lzM2730YK3">
+              What is Backstage?
+            </button>
+          </li>
+          <li>
+            <button data-spotify-id="spotify:episode:6I3ZzCxRhRkNqnQNo8AZPV">
+              Introducing Nerd Out@Spotify
+            </button>
+          </li>
+        </ul>
+
+        <div id="embed-iframe"></div>
+
+      </Container>
+
     </div>
   );
 }
---
+++ src/components/AppBar.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function MenuAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" component="div" align="left" sx={{ flexGrow: 1 }}>
+            AI-ify
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
